83b2a42f8b1a8f19feda0f34538c1b57ca6af729
Walkerjony <55608271+Walkerjony@users.noreply.github.com>
2021-06-08 06:48:37 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,17 +2,17 @@ def Fibonacci(n):
     print('-'*30)
     print('Fibonnaci')
     print('-'*30)
-    n = int(input('Digite os termos: '))
     t1 = 0
     t2 = 1
+    print(f'{t1} - {t2}', end='')
     cont = 3
     while cont <= n:
         t3 = t1 + t2
         print(f' - {t3}', end='')
-        t1 = t2 
+        t1 = t2
         t2 = t3
         cont += 1
 
-n1 = int(input)
+n1 = int(input('Input number: '))
 
 Fibonacci(n1)
